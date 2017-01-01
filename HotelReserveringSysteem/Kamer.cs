048d424b5ab30f538648466255512d5821cae7ee
Katherine Manders <Katherine Manders@DESKTOP-EAHMV71>
2017-01-01 04:31:49 -0800

    Add project files.

---
+++ HotelReserveringSysteem/Kamer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotelReserveringSysteem
+{
+    public class Kamer
+    {
+        private int kamerNR;
+        private int personenPerKamer;
+
+        public int KamerNR
+        {
+            get
+            {
+                return kamerNR;
+            }
+            set
+            {
+                kamerNR = value;
+            }
+        }
+        public int PersonenPerKamer
+        {
+            get
+            {
+                return personenPerKamer;
+            }
+            set
+            {
+                personenPerKamer = value;
+            }
+        }
+
+        #region methodes
+
+        #endregion
+    }
+}
