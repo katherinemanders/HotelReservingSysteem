e7f8d069a37f76b1798a6fb131e2e2e086c1bc73
Tim <tim.kruijsen@gmail.com>
2017-01-09 22:24:48 +0100

    Opslaan in .txt bestanden Deels uitlezen van .txt bestanden Bezetting status laten zien per dag Klein beetje UI Kamer klasse flink uitgebreid

--- HotelReserveringSysteem/Kamer.cs
+++ HotelReserveringSysteem/Kamer.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using Windows.Storage;
 
 namespace HotelReserveringSysteem
 {
@@ -9,6 +12,11 @@ namespace HotelReserveringSysteem
     {
         private int kamerNR;
         private int personenPerKamer;
+        private string[] bezettingDitJaar;
+        private string[] aantalPersonenDitJaar;
+        private string[] gegevensDitJaar;
+        private string[] dinerOntbijtDitJaar;
+        private string fileName;
 
         public int KamerNR
         {
@@ -32,9 +40,147 @@ namespace HotelReserveringSysteem
                 personenPerKamer = value;
             }
         }
+        public string[] BezettingDitJaar
+        {
+            get
+            {
+                return bezettingDitJaar;
+            }
+            set
+            {
+                bezettingDitJaar = value;
+            }
+        }
+        public string[] GegevensDitJaar
+        {
+            get
+            {
+                return gegevensDitJaar;
+            }
+            set
+            {
+                gegevensDitJaar = value;
+            }
+        }
+        public string[] DinerOntbijtDitJaar
+        {
+            get
+            {
+                return dinerOntbijtDitJaar;
+            }
+            set
+            {
+                dinerOntbijtDitJaar = value;
+            }
+        }
+        public string[] AantalPersonenDitJaar
+        {
+            get
+            {
+                return aantalPersonenDitJaar;
+            }
+            set
+            {
+                aantalPersonenDitJaar = value;
+            }
+        }
+
+        public Kamer(int nummer)
+        {
+            PersonenPerKamer = 4;
+            KamerNR = nummer;
+            
+            GetData();
+        }
 
         #region methodes
+        private async void GetData()
+        {
+            StorageFolder appFolder = ApplicationData.Current.LocalFolder;
 
+            fileName = "Kamer" + KamerNR + DateTime.Now.Year + ".txt";
+
+            StorageFile kamerBestand = await appFolder.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists);
+
+            string text = await FileIO.ReadTextAsync(kamerBestand);
+
+            if (text.Split(';').Count() < 3)
+            {
+                string tempString = string.Empty;
+
+                for (int i = 1; i <= (DateTime.Now.Year % 4 == 0 ? 366 : 365); i++)
+                {
+                    //bezetting
+                    tempString += "vrij,";
+                }
+
+                tempString = tempString.Remove(tempString.Length - 1, 1) + ";";
+
+                for (int i = 1; i <= (DateTime.Now.Year % 4 == 0 ? 366 : 365); i++)
+                {
+                    //aantal personen
+                    tempString += "0(";
+
+                    for (int j = 0; j < PersonenPerKamer; j++)
+                    {
+                        //persoonsgegevens
+                        tempString += "naam|adres|woonplaats|leeftijd|geslacht&";
+                    }
+
+                    tempString = tempString.Remove(tempString.Length - 1, 1) + ");";
+                }
+
+                for (int i = 1; i <= (DateTime.Now.Year % 4 == 0 ? 366 : 365); i++)
+                {
+                    //ontbijt/diner aantal personen
+                    tempString += "0/0,";
+                }
+
+                tempString = tempString.Remove(tempString.Length - 1, 1);
+
+                await FileIO.WriteTextAsync(kamerBestand, tempString);
+            }
+
+            GetBezettingDitJaarVanBestand();
+        }
+
+        private async void GetBezettingDitJaarVanBestand()
+        {
+            StorageFolder appFolder = ApplicationData.Current.LocalFolder;
+
+            fileName = "Kamer" + KamerNR + DateTime.Now.Year + ".txt";
+
+            StorageFile kamerBestand = await appFolder.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists);
+
+            string text = await FileIO.ReadTextAsync(kamerBestand);
+
+            BezettingDitJaar = text.Split(';')[0].Split(',');
+            AantalPersonenDitJaar = text.Split(';')[1].Split('(');
+
+            for (int i = 0; i < AantalPersonenDitJaar.Count(); i++)
+            {
+                AantalPersonenDitJaar[i] = AantalPersonenDitJaar[i].Substring(AantalPersonenDitJaar[i].Length - 1);
+            }
+
+            Debug.WriteLine("Data opgehaald voor: " + KamerNR);
+        }
+
+        public async void SetBezetting(int dag)
+        {
+            BezettingDitJaar[dag - 1] = "bezet";
+
+            StorageFolder appFolder = ApplicationData.Current.LocalFolder;
+
+            fileName = "Kamer" + KamerNR + DateTime.Now.Year + ".txt";
+
+            StorageFile kamerBestand = await appFolder.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists);
+
+            string text = await FileIO.ReadTextAsync(kamerBestand);
+
+            string tempString = string.Join(",", BezettingDitJaar);
+
+            await FileIO.WriteTextAsync(kamerBestand, tempString + ";" + text.Split(';')[1] + ";" + text.Split(';')[2]);
+        }
         #endregion
     }
 }
--- HotelReserveringSysteem/MainPage.xaml.cs
+++ HotelReserveringSysteem/MainPage.xaml.cs
@@ -22,9 +22,59 @@ namespace HotelReserveringSysteem
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Kamer[] kamers;
+        int dayNumber = 1;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            kamers = new Kamer[312];
+
+            int j = 0;
+
+            for (int i = 1; i <= 313; i++)
+            {
+                if (i != 13)
+                {
+                    kamers[j] = new Kamer(i);
+                    j++;
+                }
+            }
+
+            textKamerBezetting.Text = "Dag in " + DateTime.Now.Year;
+        }
+
+        private void buttonBookRoom_Click(object sender, RoutedEventArgs e)
+        {
+            kamers[0].SetBezetting(5);
+        }
+
+        private void textBoxDayNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int.TryParse(textBoxDayNumber.Text, out dayNumber);
+
+            if (dayNumber > 0 && dayNumber <= kamers[0].BezettingDitJaar.Count())
+            {
+                RefreshBezetting();
+            }
+        }
+
+        private void RefreshBezetting()
+        {
+            listViewBezetting.Items.Clear();
+
+            foreach (Kamer kamer in kamers)
+            {
+                listViewBezetting.Items.Add(kamer.KamerNR + " - " + kamer.BezettingDitJaar[dayNumber - 1]);
+            }
+        }
+
+        private void listViewBezetting_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            textBoxReserveringInfo.Text = "Kamer nummer: " + kamers[listViewBezetting.SelectedIndex].KamerNR + "\n" +
+                "Aantal personen: " + /*kamers[listViewBezetting.SelectedIndex].AantalPersonenDitJaar[dayNumber - 1] +*/ "/" + kamers[listViewBezetting.SelectedIndex].PersonenPerKamer + "\n" +
+                "";
         }
     }
 }
