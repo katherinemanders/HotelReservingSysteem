8067656546d0789b9483bd943e5dbe190d90ad9f
Katherine Manders <katherinemanders@gmail.com>
2017-01-10 11:52:58 -0800

    Toevoeging van klassen

---
+++ HotelReserveringSysteem/Personen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReserveringSysteem
+{
+    class Personen
+    {
+        private string voornaam;
+
+        public string Voornaam
+        {
+            get { return voornaam; }
+            set { voornaam = value; }
+        }
+
+        private string achternaam;
+
+        public string Achternaam
+        {
+            get { return achternaam; }
+            set { achternaam = value; }
+        }
+
+
+
+
+    }
+}
